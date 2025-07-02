283780ba554bf5c0c20f041ab3857b4b8ad05636
Hailey Burgess <hailey.burgess@redcross.org>
2025-07-02 18:59:28 -0400

    working through tests, routes all in place

--- api/tracks.js
+++ api/tracks.js
@@ -4,12 +4,15 @@ export default router;
 
 import { getAllTracks } from "#db/queries/tracks";
 import { getTracksById } from "#db/queries/tracks";
-import { getTracksByPlaylistId } from "#db/queries/tracks";
+
+// Get all tracks
 
 router.route("/").get(async (request, response) => {
   const tracks = await getAllTracks();
   response.send(tracks);
 });
+
+// Get tracks by ID (params)
 
 router.route("/:id").get(async (request, response) => {
   const { id } = request.params;
